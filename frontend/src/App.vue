1096b8ac7c3196215981cc0bbd45e4f24c3f80e8
Lars Rickert <dev@lars-rickert.de>
2023-03-25 16:03:55 +0100

    remove dark mode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,6 @@
 <script lang="ts" setup>
-import { useDark } from "@vueuse/core";
 import { RouterView } from "vue-router";
 import BackgroundAnimationAtom from "./components/atoms/BackgroundAnimationAtom.vue";
-
-// enable automatic dark mode based on user preferences
-useDark();
 </script>
 
 <template>
